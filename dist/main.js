9edce39eb6d85ff20403fb5e4269a78b481747e7
Vladislav Ivanov <himametsu@gmail.com>
2020-04-09 18:25:19 +0300

    feat: update final js files and styles

--- dist/main.js
+++ dist/main.js
@@ -195,7 +195,7 @@ var CallMenu = /*#__PURE__*/function () {
       menu.classList.toggle('openMenu');
       var header = document.getElementsByClassName('header')[0];
       var menuItems = document.getElementsByClassName('menu-items')[0];
-      menuItems.setAttribute('style', "margin-top: ".concat(header.clientHeight + header.offsetTop + 10, "px"));
+      menuItems.setAttribute('style', "margin-top: ".concat(header.clientHeight + header.offsetTop + 30, "px"));
     }
   }]);
 
@@ -247,10 +247,13 @@ var Menu = /*#__PURE__*/function () {
       var list = document.createElement('ul');
       list.classList.add('menu-items');
       menu.classList.add('menu');
-      var listItems = ['Main Page', 'Action (Set A)', 'Action (Set B)', 'Action (Set C)', 'Animal (Set A)', 'Animal (Set B)', 'Adjective', 'Clothes'];
+      var listItems = ['Main Page', 'Action (Set A)', 'Action (Set B)', 'Action (Set C)', 'Animal (Set A)', 'Animal (Set B)', 'Adjective', 'Clothes', 'Emotions'];
       listItems.map(function (item) {
         var link = document.createElement('li');
         var a = document.createElement('a');
+        a.addEventListener('click', function () {
+          Menu.selectedSection();
+        });
         link.classList.add('item');
         var textLink = document.createTextNode("".concat(item));
         a.appendChild(textLink);
@@ -260,18 +263,432 @@ var Menu = /*#__PURE__*/function () {
       menu.appendChild(list);
       return menu;
     }
+  }, {
+    key: "selectedSection",
+    value: function selectedSection() {
+      console.log("\u0412\u044B\u0431\u0440\u0430\u043D \u0440\u0430\u0437\u0434\u0435\u043B: ".concat(event.target.innerText));
+      var allTd = Array.from(document.getElementsByClassName('td'));
+      allTd.map(function (td) {
+        return td.classList.remove('td');
+      });
+      event.target.classList.add('td');
+      var menuBtn = document.getElementsByClassName('menu openMenu');
+      var sections = Array.from(document.getElementsByClassName('section'));
+      menuBtn[0].classList.remove('openMenu');
+      sections.map(function (section) {
+        return section.classList.add('hidden');
+      });
+      document.getElementsByClassName(event.target.innerText.replace(/\s/ig, '-'))[0].classList.remove('hidden');
+    }
   }]);
 
   return Menu;
+}();
+// CONCATENATED MODULE: ./src/DATA/cards.js
+var cards = [['Action (set A)', 'Action (set B)', 'Animal (set A)', 'Animal (set B)', 'Clothes', 'Emotions'], [{
+  word: 'cry',
+  translation: 'плакать',
+  image: 'img/cry.jpg',
+  audioSrc: 'audio/cry.mp3'
+}, {
+  word: 'dance',
+  translation: 'танцевать',
+  image: 'img/dance.jpg',
+  audioSrc: 'audio/dance.mp3'
+}, {
+  word: 'dive',
+  translation: 'нырять',
+  image: 'img/dive.jpg',
+  audioSrc: 'audio/dive.mp3'
+}, {
+  word: 'draw',
+  translation: 'рисовать',
+  image: 'img/draw.jpg',
+  audioSrc: 'audio/draw.mp3'
+}, {
+  word: 'fish',
+  translation: 'ловить рыбу',
+  image: 'img/fish.jpg',
+  audioSrc: 'audio/fish.mp3'
+}, {
+  word: 'fly',
+  translation: 'летать',
+  image: 'img/fly.jpg',
+  audioSrc: 'audio/fly.mp3'
+}, {
+  word: 'hug',
+  translation: 'обнимать',
+  image: 'img/hug.jpg',
+  audioSrc: 'audio/hug.mp3'
+}, {
+  word: 'jump',
+  translation: 'прыгать',
+  image: 'img/jump.jpg',
+  audioSrc: 'audio/jump.mp3'
+}], [{
+  word: 'open',
+  translation: 'открывать',
+  image: 'img/open.jpg',
+  audioSrc: 'audio/open.mp3'
+}, {
+  word: 'play',
+  translation: 'играть',
+  image: 'img/play.jpg',
+  audioSrc: 'audio/play.mp3'
+}, {
+  word: 'point',
+  translation: 'указывать',
+  image: 'img/point.jpg',
+  audioSrc: 'audio/point.mp3'
+}, {
+  word: 'ride',
+  translation: 'ездить',
+  image: 'img/ride.jpg',
+  audioSrc: 'audio/ride.mp3'
+}, {
+  word: 'run',
+  translation: 'бегать',
+  image: 'img/run.jpg',
+  audioSrc: 'audio/run.mp3'
+}, {
+  word: 'sing',
+  translation: 'петь',
+  image: 'img/sing.jpg',
+  audioSrc: 'audio/sing.mp3'
+}, {
+  word: 'skip',
+  translation: 'пропускать, прыгать',
+  image: 'img/skip.jpg',
+  audioSrc: 'audio/skip.mp3'
+}, {
+  word: 'swim',
+  translation: 'плавать',
+  image: 'img/swim.jpg',
+  audioSrc: 'audio/swim.mp3'
+}], [{
+  word: 'cat',
+  translation: 'кот',
+  image: 'img/cat.jpg',
+  audioSrc: 'audio/cat.mp3'
+}, {
+  word: 'chick',
+  translation: 'цыплёнок',
+  image: 'img/chick.jpg',
+  audioSrc: 'audio/chick.mp3'
+}, {
+  word: 'chicken',
+  translation: 'курица',
+  image: 'img/chicken.jpg',
+  audioSrc: 'audio/chicken.mp3'
+}, {
+  word: 'dog',
+  translation: 'собака',
+  image: 'img/dog.jpg',
+  audioSrc: 'audio/dog.mp3'
+}, {
+  word: 'horse',
+  translation: 'лошадь',
+  image: 'img/horse.jpg',
+  audioSrc: 'audio/horse.mp3'
+}, {
+  word: 'pig',
+  translation: 'свинья',
+  image: 'img/pig.jpg',
+  audioSrc: 'audio/pig.mp3'
+}, {
+  word: 'rabbit',
+  translation: 'кролик',
+  image: 'img/rabbit.jpg',
+  audioSrc: 'audio/rabbit.mp3'
+}, {
+  word: 'sheep',
+  translation: 'овца',
+  image: 'img/sheep.jpg',
+  audioSrc: 'audio/sheep.mp3'
+}], [{
+  word: 'bird',
+  translation: 'птица',
+  image: 'img/bird.jpg',
+  audioSrc: 'audio/bird.mp3'
+}, {
+  word: 'fish',
+  translation: 'рыба',
+  image: 'img/fish1.jpg',
+  audioSrc: 'audio/fish.mp3'
+}, {
+  word: 'frog',
+  translation: 'жаба',
+  image: 'img/frog.jpg',
+  audioSrc: 'audio/frog.mp3'
+}, {
+  word: 'giraffe',
+  translation: 'жирафа',
+  image: 'img/giraffe.jpg',
+  audioSrc: 'audio/giraffe.mp3'
+}, {
+  word: 'lion',
+  translation: 'лев',
+  image: 'img/lion.jpg',
+  audioSrc: 'audio/lion.mp3'
+}, {
+  word: 'mouse',
+  translation: 'мышь',
+  image: 'img/mouse.jpg',
+  audioSrc: 'audio/mouse.mp3'
+}, {
+  word: 'turtle',
+  translation: 'черепаха',
+  image: 'img/turtle.jpg',
+  audioSrc: 'audio/turtle.mp3'
+}, {
+  word: 'dolphin',
+  translation: 'дельфин',
+  image: 'img/dolphin.jpg',
+  audioSrc: 'audio/dolphin.mp3'
+}], [{
+  word: 'skirt',
+  translation: 'юбка',
+  image: 'img/skirt.jpg',
+  audioSrc: 'audio/skirt.mp3'
+}, {
+  word: 'pants',
+  translation: 'брюки',
+  image: 'img/pants.jpg',
+  audioSrc: 'audio/pants.mp3'
+}, {
+  word: 'blouse',
+  translation: 'блузка',
+  image: 'img/blouse.jpg',
+  audioSrc: 'audio/blouse.mp3'
+}, {
+  word: 'dress',
+  translation: 'платье',
+  image: 'img/dress.jpg',
+  audioSrc: 'audio/dress.mp3'
+}, {
+  word: 'boot',
+  translation: 'ботинок',
+  image: 'img/boot.jpg',
+  audioSrc: 'audio/boot.mp3'
+}, {
+  word: 'shirt',
+  translation: 'рубашка',
+  image: 'img/shirt.jpg',
+  audioSrc: 'audio/shirt.mp3'
+}, {
+  word: 'coat',
+  translation: 'пальто',
+  image: 'img/coat.jpg',
+  audioSrc: 'audio/coat.mp3'
+}, {
+  word: 'shoe',
+  translation: 'туфли',
+  image: 'img/shoe.jpg',
+  audioSrc: 'audio/shoe.mp3'
+}], [{
+  word: 'sad',
+  translation: 'грустный',
+  image: 'img/sad.jpg',
+  audioSrc: 'audio/sad.mp3'
+}, {
+  word: 'angry',
+  translation: 'сердитый',
+  image: 'img/angry.jpg',
+  audioSrc: 'audio/angry.mp3'
+}, {
+  word: 'happy',
+  translation: 'счастливый',
+  image: 'img/happy.jpg',
+  audioSrc: 'audio/happy.mp3'
+}, {
+  word: 'tired',
+  translation: 'уставший',
+  image: 'img/tired.jpg',
+  audioSrc: 'audio/tired.mp3'
+}, {
+  word: 'surprised',
+  translation: 'удивлённый',
+  image: 'img/surprised.jpg',
+  audioSrc: 'audio/surprised.mp3'
+}, {
+  word: 'scared',
+  translation: 'испуганный',
+  image: 'img/scared.jpg',
+  audioSrc: 'audio/scared.mp3'
+}, {
+  word: 'smile',
+  translation: 'улыбка',
+  image: 'img/smile.jpg',
+  audioSrc: 'audio/smile.mp3'
+}, {
+  word: 'laugh',
+  translation: 'смех',
+  image: 'img/laugh.jpg',
+  audioSrc: 'audio/laugh.mp3'
+}]];
+// CONCATENATED MODULE: ./src/UI/card/card.js
+function card_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function card_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function card_createClass(Constructor, protoProps, staticProps) { if (protoProps) card_defineProperties(Constructor.prototype, protoProps); if (staticProps) card_defineProperties(Constructor, staticProps); return Constructor; }
+
+var Card = /*#__PURE__*/function () {
+  function Card() {
+    card_classCallCheck(this, Card);
+  }
+
+  card_createClass(Card, null, [{
+    key: "createCard",
+    value: function createCard(word, path) {
+      var card = document.createElement('div');
+      card.classList.add('card');
+      var figure = document.createElement('figure');
+      var img = document.createElement('img');
+
+      if (path) {
+        img.src = "";
+      } else {
+        img.src = "../../../dist/img/".concat(word, ".jpg");
+      }
+
+      var figcaption = document.createElement('figcaption');
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      var textFigure = document.createTextNode("".concat(word));
+      figcaption.appendChild(textFigure);
+      card.appendChild(figure);
+      return card;
+    }
+  }]);
+
+  return Card;
+}();
+// CONCATENATED MODULE: ./src/modules/section.js
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function section_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function section_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function section_createClass(Constructor, protoProps, staticProps) { if (protoProps) section_defineProperties(Constructor.prototype, protoProps); if (staticProps) section_defineProperties(Constructor, staticProps); return Constructor; }
+
+
+
+var section_Section = /*#__PURE__*/function () {
+  function Section() {
+    section_classCallCheck(this, Section);
+  }
+
+  section_createClass(Section, null, [{
+    key: "createSection",
+    value: function createSection() {
+      var allSections = Array.apply(void 0, _toConsumableArray(cards[0]));
+      var allCards = [];
+      cards.map(function (item, i) {
+        if (cards[i + 1]) {
+          allCards.push(cards[i + 1]);
+        }
+      });
+      var sections = document.createElement('div');
+      sections.classList.add('sections');
+      allSections.map(function (item, i) {
+        var section = document.createElement('div');
+        allCards[i].map(function (card) {
+          section.appendChild(Card.createCard(card.word));
+        });
+        section.classList.add('section');
+        section.classList.add('hidden');
+        section.classList.add("".concat(item.replace(/\s/ig, '-')));
+        sections.appendChild(section);
+      });
+      return sections;
+    }
+  }]);
+
+  return Section;
+}();
+// CONCATENATED MODULE: ./src/modules/main-content.js
+function main_content_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function main_content_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function main_content_createClass(Constructor, protoProps, staticProps) { if (protoProps) main_content_defineProperties(Constructor.prototype, protoProps); if (staticProps) main_content_defineProperties(Constructor, staticProps); return Constructor; }
+
+
+var main_content_MainContent = /*#__PURE__*/function () {
+  function MainContent() {
+    main_content_classCallCheck(this, MainContent);
+  }
+
+  main_content_createClass(MainContent, null, [{
+    key: "createContent",
+    value: function createContent() {
+      var content = document.createElement('div');
+      content.classList.add('main-content');
+      content.appendChild(section_Section.createSection());
+      return content;
+    }
+  }]);
+
+  return MainContent;
+}();
+// CONCATENATED MODULE: ./src/modules/main-page.js
+function main_page_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function main_page_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function main_page_createClass(Constructor, protoProps, staticProps) { if (protoProps) main_page_defineProperties(Constructor.prototype, protoProps); if (staticProps) main_page_defineProperties(Constructor, staticProps); return Constructor; }
+
+
+
+var main_page_MainPage = /*#__PURE__*/function () {
+  function MainPage() {
+    main_page_classCallCheck(this, MainPage);
+  }
+
+  main_page_createClass(MainPage, null, [{
+    key: "createMainPage",
+    value: function createMainPage() {
+      var mainPage = document.createElement('div');
+      mainPage.classList.add('main-page');
+      var allCategories = [];
+      cards.map(function (item, i) {
+        if (cards[0][i]) {
+          allCategories.push(cards[0][i]);
+        }
+      });
+      allCategories.map(function (category) {
+        mainPage.appendChild(Card.createCard(category));
+      });
+      return mainPage;
+    }
+  }]);
+
+  return MainPage;
 }();
 // CONCATENATED MODULE: ./src/App.js
 
 
 
+
+
 document.body.appendChild(Container.createContainer());
 var container = document.getElementsByClassName('container')[0];
-container.appendChild(header_Header.createHeader());
 container.append(Menu.createMenu());
+container.appendChild(header_Header.createHeader());
+container.appendChild(main_content_MainContent.createContent());
+container.appendChild(main_page_MainPage.createMainPage());
 
 /***/ })
 /******/ ]);
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Container } from './modules/container'
 import { Header } from './modules/header'
 import { Menu } from './modules/menu'
+import { MainContent } from './modules/main-content'
+import { MainPage } from './modules/main-page'
 
 document.body.appendChild(Container.createContainer())
 const container = document.getElementsByClassName('container')[0]
-container.appendChild(Header.createHeader())
 container.append(Menu.createMenu())
+container.appendChild(Header.createHeader())
+container.appendChild(MainContent.createContent())
+container.appendChild(MainPage.createMainPage())
