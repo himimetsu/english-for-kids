023c31174c8fff919498440b0eca69c2581306c1
Vladislav Ivanov <himametsu@gmail.com>
2020-04-09 12:11:56 +0300

    feat: changed final file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/App.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
