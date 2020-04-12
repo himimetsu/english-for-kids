e67a9c02dd0239e8fc32d6bb529f636f246302d5
Vladislav Ivanov <himametsu@gmail.com>
2020-04-12 14:56:18 +0300

    feat: add checking the current state of the game

--- src/UI/switchBtn/modeSwitch.js
+++ src/UI/switchBtn/modeSwitch.js
@@ -3,6 +3,23 @@ const toggleMenu = () => {
   menu.classList.remove('openMenu')
 }
 
+const setFlag = (event) => {
+  if (event.propertyName === 'right') {
+    document.getElementsByClassName('main-content')[0].classList.toggle('play')
+    if (document.getElementsByClassName('main-page')[0].className === 'main-page hidden') {
+      document.getElementsByClassName('game-button')[0].classList.toggle('hidden')
+    }
+
+    const allGamesCards = Array.from(document.getElementsByClassName('sections')[0].getElementsByClassName('card'))
+    allGamesCards.map((gameCard) => {
+      gameCard.classList.toggle('hide-tooltips')
+      gameCard.classList.toggle('block-anima')
+
+      return true
+    })
+  }
+}
+
 const createInput = () => {
   const input = document.createElement('input')
   input.classList.add('onoffswitch-checkbox')
@@ -33,7 +50,8 @@ const createBtn = () => {
   modeSwitch.classList.add('onoffswitch')
   modeSwitch.appendChild(createInput())
   modeSwitch.appendChild(createLabel())
-  modeSwitch.addEventListener('click', toggleMenu())
+  modeSwitch.addEventListener('click', toggleMenu)
+  modeSwitch.addEventListener('transitionend', setFlag)
 
   return modeSwitch
 }
