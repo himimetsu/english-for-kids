ecff2da071ecc5a64fe36828b9e61c7e8e2caeef
Vladislav Ivanov <himametsu@gmail.com>
2020-04-12 14:53:07 +0300

    feat: add game start button

---
+++ src/UI/gameButton/gameButton.js
@@ -0,0 +1,20 @@
+const startGame = () => {
+  console.log('Game started!')
+}
+
+const gameButton = () => {
+  const button = document.createElement('button')
+  button.classList.add('game-button')
+  button.classList.add('hidden')
+  button.setAttribute('type', 'button')
+  button.appendChild(document.createTextNode('START GAME'))
+  button.addEventListener('click', startGame)
+  const shellButton = document.createElement('div')
+  shellButton.classList.add('shell-button')
+  shellButton.appendChild(button)
+
+  return shellButton
+}
+
+const createGameButton = () => gameButton()
+export default createGameButton
