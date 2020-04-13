e6f30a127661dba0b7f15ce2555c32ca9734ceae
Vladislav Ivanov <himametsu@gmail.com>
2020-04-13 16:43:12 +0300

    feat: add shuffling an array of words

--- src/UI/gameButton/gameButton.js
+++ src/UI/gameButton/gameButton.js
@@ -3,19 +3,33 @@ import hideMenu from '../../scripts/hideMenu'
 import player from '../../scripts/audioPlayer'
 import searchSection from '../../scripts/searchActiveSection'
 import shuffle from '../../scripts/shuffle'
+
+const createGameArray = () => {
+  document.getElementsByClassName(searchSection())[0].getElementsByClassName('shell-button')[0].getElementsByClassName('game-button')[0].classList.add('repeat')
+  let gameArray = []
+  cards[1][cards[0][1].indexOf(searchSection().replace(/-/igu, ' '))].map((item) => {
+    gameArray.push(item.word)
+
+    return true
+  })
+  gameArray = shuffle(gameArray)
+  console.log(gameArray)
+}
+
 const startGame = () => {
-  console.log('Game started!')
+  hideMenu()
+  createGameArray()
 }
 
 const gameButton = () => {
   const button = document.createElement('button')
   button.classList.add('game-button')
-  button.classList.add('hidden')
   button.setAttribute('type', 'button')
   button.appendChild(document.createTextNode('START GAME'))
   button.addEventListener('click', startGame)
   const shellButton = document.createElement('div')
   shellButton.classList.add('shell-button')
+  shellButton.classList.add('hidden')
   shellButton.appendChild(button)
 
   return shellButton
