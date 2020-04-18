5dd751bc7960c6822a6daf9a7727cd8a64ebc2f1
Vladislav Ivanov <himametsu@gmail.com>
2020-04-18 23:38:33 +0300

    feat: add create local store

---
+++ src/scripts/LocalStore.js
@@ -0,0 +1,19 @@
+import cards from '../DATA/cards'
+
+let iter = 0
+const createLocalStore = () => {
+  if (localStorage.key('LocalStatistics') === null) {
+    localStorage.setItem('LocalStatistics', '')
+    cards[0][1].map((section, index) => {
+      cards[1][index].map((item) => {
+        localStorage.setItem(`statistics${iter}`, [section, item.word, item.translation, 0, 0, 0, 0])
+        iter += 1
+
+        return true
+      })
+
+      return true
+    })
+  }
+}
+export default createLocalStore
