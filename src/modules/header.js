63346b729423178df446ffe8238a20dae6ae27fa
Vladislav Ivanov <himametsu@gmail.com>
2020-04-18 23:26:48 +0300

    feat: add render button for call stat

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,10 +1,12 @@
 import createCallMenu from '../UI/callMenu/callMenu'
 import createSwitch from '../UI/switchBtn/modeSwitch'
+import openStat from '../UI/OpenStatBtn/OpenStatBtn'
 
 const createHeader = () => {
   const header = document.createElement('div')
   header.classList.add('header')
   header.appendChild(createCallMenu())
+  header.appendChild(openStat())
   header.appendChild(createSwitch())
 
   return header
