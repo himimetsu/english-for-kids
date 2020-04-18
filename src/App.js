7fa7cbae267fc8019c628c70aed395740e6a445d
Vladislav Ivanov <himametsu@gmail.com>
2020-04-18 23:26:13 +0300

    feat: add render statistic block and create local-store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import cards from './DATA/cards'
 import createContainer from './modules/container'
 import createHeader from './modules/header'
+import createLocalStore from './scripts/LocalStore'
 import createMainContent from './modules/main-content'
 import createMainPage from './modules/main-page'
 import createMenu from './modules/menu'
+import createStatistics from './modules/statistics'
 import modal from './modules/Modal'
 
 document.body.appendChild(createContainer())
@@ -13,3 +15,5 @@ container.appendChild(createHeader())
 container.appendChild(createMainContent(cards[0][1], cards[1]))
 container.appendChild(createMainPage(cards[0][1]))
 container.appendChild(modal())
+createLocalStore()
+container.appendChild(createStatistics())
