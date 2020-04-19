5d9ef7edbe7e3112acb99faab0710932daec1f23
Vladislav Ivanov <himametsu@gmail.com>
2020-04-19 14:43:40 +0300

    feat: change button call-menu

--- src/UI/callMenu/callMenu.js
+++ src/UI/callMenu/callMenu.js
@@ -1,9 +1,20 @@
+const lineBtn = () => {
+  const frag = document.createDocumentFragment()
+  for (let iter = 0; iter < 3; iter += 1) {
+    const span = document.createElement('span')
+    frag.appendChild(span)
+  }
+
+  return frag
+}
+
 const createCallMenu = () => {
   const callMenu = document.createElement('div')
+  callMenu.appendChild(lineBtn())
   callMenu.classList.add('btn-call-menu')
   callMenu.addEventListener('click', () => {
     const tempMenu = document.getElementsByClassName('btn-call-menu')[0]
-    tempMenu.classList.toggle('active')
+    tempMenu.classList.toggle('open')
     const menu = document.getElementsByClassName('menu')[0]
     menu.classList.toggle('openMenu')
     const header = document.getElementsByClassName('header')[0]
