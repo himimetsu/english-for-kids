0711a22ac48a6c9519815c73db021015d056480d
Vladislav Ivanov <himametsu@gmail.com>
2020-04-19 21:55:10 +0300

    feat: add field for stars

--- src/modules/main-content.js
+++ src/modules/main-content.js
@@ -1,8 +1,16 @@
 import createSection from './section'
 
+const createResultStar = () => {
+  const div = document.createElement('div')
+  div.classList.add('field-stars')
+
+  return div
+}
+
 const createMainContent = (arrSections, arrCards) => {
   const content = document.createElement('div')
   content.classList.add('main-content')
+  content.appendChild(createResultStar())
   content.appendChild(createSection(arrSections, arrCards, false))
 
   return content
