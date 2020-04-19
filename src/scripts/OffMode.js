e2af2dc82babb3663084be8108e99c7216b592c9
Vladislav Ivanov <himametsu@gmail.com>
2020-04-19 21:58:38 +0300

    feat: add zeroing the field of stars

--- src/scripts/OffMode.js
+++ src/scripts/OffMode.js
@@ -1,6 +1,13 @@
 import refreshGameBtn from './RefreshGameBtn'
 
+const clearFieldStars = () => {
+  document.getElementsByClassName('btn-call-menu')[0].classList.remove('open')
+  const stars = Array.from(document.getElementsByClassName('field-stars')[0].children)
+  stars.map((star) => star.remove())
+}
+
 const off = (key) => {
+  clearFieldStars()
   const allNonClick = Array.from(document.getElementsByClassName('non-click'))
   allNonClick.map((non) => non.classList.remove('non-click'))
   const allUsed = Array.from(document.getElementsByClassName('used'))
