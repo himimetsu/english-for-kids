9e26c953d4ee173c3824e04c0a4a0fed98bb2a9f
Vladislav Ivanov <himametsu@gmail.com>
2020-04-19 21:57:38 +0300

    feat: add interest calculation

--- src/scripts/LocalStatistics.js
+++ src/scripts/LocalStatistics.js
@@ -1,11 +1,14 @@
+const percent = (currentStat) => (100 / (currentStat[3] / 1 + currentStat[4] / 1)) * currentStat[4]
+
 const stat = (section, eng, arr) => {
   const currentStat = localStorage.getItem(eng).split(',')
-  console.log(localStorage.getItem(eng))
   if (arr[0] === 'game') {
     if (arr[1] === 'right') {
       currentStat[3] = currentStat[3] / 1 + 1
+      currentStat[6] = percent(currentStat).toFixed()
     } else if (arr[1] === 'wrong') {
       currentStat[4] = currentStat[4] / 1 + 1
+      currentStat[6] = percent(currentStat).toFixed()
     }
   } else if (arr[0] === 'train') {
     currentStat[5] = currentStat[5] / 1 + 1
